03a1820ea4e720f26ad1ca0ce73dcc9fa8034079
Musaddique Shaikh <125135256+mrMusaddique@users.noreply.github.com>
2024-02-24 18:26:26 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+ var button = document.querySelector(".darktheme");
+
+
+button.onclick = function(){
+    document.querySelector(".darktheme").classList.toggle("active");
+    document.body.classList.toggle("dark-theme-color");
+}
+
+alert("assalamu alaikum wellcome to Musaddique'Website ");
